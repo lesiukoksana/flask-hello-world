74917d11e66adb4d67e67309885dda13d43441ca
lesiukoksana <leeeesiuk@gmail.com>
2025-12-09 01:36:17 +0000

    Update

--- app.py
+++ app.py
@@ -3,8 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-    return 'Hello, World! edited'
-=======
-    return 'Hello, World! edit for docker'
->>>>>>> 0e4e888 (Changes for Docker)
+    return 'Hello, World! edit again and again'
